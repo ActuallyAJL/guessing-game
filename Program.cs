0c270669d7376de39454949fe28ec091e6e101b0
ActuallyAJL <zero3260@hotmail.com>
2022-05-23 13:45:26 -0500

    phase 7 complete

--- Program.cs
+++ Program.cs
@@ -6,14 +6,36 @@ namespace guessing_game
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Welcome to secret number. shhhhh...");
+            Console.WriteLine("Choose a difficulty. Type: ");
+            Console.WriteLine("E-easy (8 guesses)  M-medium (6 guesses)  H-hard (4 guesses)");
+            string diff = Console.ReadLine().ToLower();
+            int guessesRemaining = 0;
+            if (diff == "e")
+            {
+                guessesRemaining = 8;
+            }
+            else if (diff == "m")
+            {
+                guessesRemaining = 6;
+            }
+            else if (diff == "h")
+            {
+                guessesRemaining = 4;
+            }
+            else
+            {
+                Console.WriteLine("huh?");
+                guessesRemaining = 0;
+            }
+
             Random r = new Random();
-            int secretNumber = r.Next(100);
-            int guessesRemaining = 4;
+            int secretNumber = r.Next(1, 101);
 
             while (guessesRemaining > 0)
             {
 
-                Console.WriteLine($"Will you guess the secret number? You have {guessesRemaining} guesses remaining.");
+                Console.WriteLine($"Will you guess the secret number between 1-100? You have {guessesRemaining} guesses remaining.");
                 int userGuess = int.Parse(Console.ReadLine());
 
                 if (userGuess == secretNumber)
